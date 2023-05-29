58b31d4521e1737a8fc352c30f3f4e1d6fc4edba
Replit user <>
2023-05-29 15:24:43 -0500

    Trabajo finalizado

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 require('dotenv').config();
+const requestIp = require('request-ip')
 var express = require('express');
 var app = express();
 
@@ -20,11 +21,15 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+   var clientIp = req.header('x-forwarded-for')
+   var languaje = req.headers["accept-language"]
+   var software = req.headers["user-agent"]
+  res.json({"ipaddress":clientIp, "language":languaje, "software":software})
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
